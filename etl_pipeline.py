d993f22333e2074eb2f90f334100ec8d7a11cc0d
callmeaash <aashishbogati9@gmail.com>
2025-08-20 22:18:51 +0545

    added extract module

--- etl_pipeline.py
+++ etl_pipeline.py
@@ -3,14 +3,17 @@ from dotenv import load_dotenv
 import os
 from pipeline_utils import extract
 
+load_dotenv()
+api_url = os.getenv('API_KEY')
+
 logging.basicConfig(
     level=logging.INFO,
     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
     handlers=[
-        logging.FileHandler('etl_pipeline.log'),
-        logging.StreamHandler()
+        logging.FileHandler('etl_pipeline.log')
     ]
 )
 
 if __name__ == "__main__":
-    extract.extract_data(os.getenv('API_KEY'))
+    raw_data = extract.extract_data(api_url)
+    
--- pipeline_utils/extract.py
+++ pipeline_utils/extract.py
@@ -1,9 +1,10 @@
-
+import requests
 import pandas as pd
 import os
 import logging
 
 logger = logging.getLogger(__name__)
+
 
 def extract_data(api_url: str) -> pd.DataFrame:
     """
@@ -19,5 +20,30 @@ def extract_data(api_url: str) -> pd.DataFrame:
 
     except FileNotFoundError:
         logger.error("Stocks file not found")
-    
+        raise
 
+    # Read data from the api for each stock, convert into Dataframe and append into a list
+    all_dfs = []
+    logger.info("Attempting to extract data...")
+    for stock in tickers['Symbol']:
+        try:
+            response = requests.get(api_url + stock)
+            data = response.json()
+            df = pd.DataFrame(data[:200])
+            all_dfs.append(df)
+
+        except requests.RequestException as e:
+            logger.error(f"Failed to fetch stock data: {e}")
+            raise
+
+        except Exception as e:
+            logger.error(f"Unexpected error occured: {e}")
+
+    # Concate the Dataframes from the list into one
+    if all_dfs:
+        combined_df = pd.concat(all_dfs, ignore_index=True)
+        logger.info(f"Combined Dataframe contains: {len(combined_df)} rows")
+        return combined_df
+    else:
+        logger.error("No stock data fetched")
+        return pd.DataFrame()
