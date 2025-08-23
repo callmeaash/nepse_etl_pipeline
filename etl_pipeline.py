7816667bb7f999d3a6a3770359d4851d97a76dbc
callmeaash <aashishbogati9@gmail.com>
2025-08-24 00:16:00 +0545

    added run_etl function

--- etl_pipeline.py
+++ etl_pipeline.py
@@ -1,21 +1,24 @@
 import logging
 from dotenv import load_dotenv
 import os
-
 from pipeline_utils import extract, transform, load
 
-load_dotenv()
-api_url = os.getenv('API_KEY')
 
-logging.basicConfig(
-    level=logging.INFO,
-    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
-    handlers=[
-        logging.FileHandler('etl_pipeline.log')
-    ]
-)
+def run_etl():
+    load_dotenv()
+    api_url = os.getenv('API_KEY')
 
-if __name__ == "__main__":
+    logging.basicConfig(
+        level=logging.INFO,
+        format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+        handlers=[
+            logging.FileHandler('etl_pipeline.log')
+        ]
+    )
+
     raw_data = extract.extract_data(api_url)
     clean_data, technical_data = transform.transform_data(raw_data)
     load.load_data(clean_data, technical_data)
+
+if __name__ == "__main__":
+    run_etl()
