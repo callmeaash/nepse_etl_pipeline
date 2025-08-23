e471068164efda30ceeab5d494c9d374e7c4dac0
callmeaash <aashishbogati9@gmail.com>
2025-08-23 19:21:51 +0545

    minor changes

---
+++ etl_pipeline.py
@@ -0,0 +1,21 @@
+import logging
+from dotenv import load_dotenv
+import os
+
+from pipeline_utils import extract, transform, load
+
+load_dotenv()
+api_url = os.getenv('API_KEY')
+
+logging.basicConfig(
+    level=logging.INFO,
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+    handlers=[
+        logging.FileHandler('etl_pipeline.log')
+    ]
+)
+
+if __name__ == "__main__":
+    raw_data = extract.extract_data(api_url)
+    clean_data, technical_data = transform.transform_data(raw_data)
+    load.load_data(clean_data, technical_data)
